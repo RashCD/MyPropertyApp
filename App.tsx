846b9e252f3be0d5af4392c5dfc501bdc8f266f1
CD <mrrash91@gmail.com>
2020-09-29 16:18:50 +0800

    Configure assets library, keyboard avoiding view

--- App.tsx
+++ App.tsx
@@ -2,6 +2,11 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import RootNavigator from 'routes/RootNavigator';
 import RootProviders from 'providers/RootProviders';
+import IconModulesAwesome from 'react-native-vector-icons/FontAwesome';
+import IconModulesAnt from 'react-native-vector-icons/AntDesign';
+
+IconModulesAwesome.loadFont();
+IconModulesAnt.loadFont();
 
 const App: React.ReactNode = () => (
   <RootProviders>
